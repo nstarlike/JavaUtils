b9e1c59bdb0ef486858001e16d753cba273809fb
nstarlike <nstar0512@gmail.com>
2023-02-12 20:25:05 +0900

    make a skeleton of IDUtil class

---
+++ src/main/java/com/nstarlike/JavaUtils/IDUtil.java
@@ -0,0 +1,15 @@
+package com.nstarlike.JavaUtils;
+
+public class IDUtil {
+	public String getRandomID() {
+		String id = "";
+		
+		return id;
+	}
+	
+	public boolean checkID(String id, String regex) {
+		boolean ret = false;
+		
+		return ret;
+	}
+}
