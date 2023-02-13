4e49210695c34d1f925f16732228cb77d8d2a556
nstarlike <nstar0512@gmail.com>
2023-02-13 20:31:23 +0900

    implement a test class for IDUtil class

--- src/test/java/com/nstarlike/JavaUtils/IDUtilTest.java
+++ src/test/java/com/nstarlike/JavaUtils/IDUtilTest.java
@@ -8,15 +8,16 @@ import org.junit.jupiter.api.Test;
 public class IDUtilTest {
 	@Test
 	public void testGetRandomId() {
-		String id = IDUtil.getRandomID();
+		String id = IDUtil.getRandomID("test");
 		
 		assertNotNull(id);
+		assertTrue(id.length() > 4);
 	}
 	
 	@Test
 	public void testCheckId() {
 		String id = "abc1234";
-		String regex = "/^[a-z0-9]{6,12}$/";
+		String regex = "^[a-z0-9]{6,20}$";
 		boolean ret = IDUtil.checkID(id, regex);
 		
 		assertTrue(ret);
