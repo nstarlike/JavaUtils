41e1a7058290a0dd5f338a447e88e5549de50305
nstarlike <nstar0512@gmail.com>
2023-02-16 16:57:22 +0900

    make the skeleton of PasswordUtilsTest

---
+++ src/test/java/nstarlike/javautils/credential/PasswordUtilsTest.java
@@ -0,0 +1,29 @@
+package nstarlike.javautils.credential;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PasswordUtilsTest {
+
+	@Test
+	void testGetRandomPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEncryptPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDecryptPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsSafePassword() {
+		fail("Not yet implemented");
+	}
+
+}
