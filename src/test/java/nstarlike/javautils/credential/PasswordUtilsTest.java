fd004d42a7c6e963e85a6f595a01ed35688b67d4
nstarlike <nstar0512@gmail.com>
2023-02-16 17:48:45 +0900

    implement a test for the getRandomPassword method of PasswordUtils

--- src/test/java/nstarlike/javautils/credential/PasswordUtilsTest.java
+++ src/test/java/nstarlike/javautils/credential/PasswordUtilsTest.java
@@ -8,7 +8,41 @@ class PasswordUtilsTest {
 
 	@Test
 	void testGetRandomPassword() {
-		fail("Not yet implemented");
+		//all English characters with only lowercases
+		int size = 10;
+		String password = PasswordUtils.getRandomPassword(size, false, false, false);
+		
+		assertNotNull(password);
+		assertTrue(password.length() == size);
+		
+		System.out.println("all lower English characters: " + password);
+		
+		//all English characters with lowercases and uppercases
+		size = 10;
+		password = PasswordUtils.getRandomPassword(size, true, false, false);
+		
+		assertNotNull(password);
+		assertTrue(password.length() == size);
+		
+		System.out.println("all lower and upper English characters: " + password);
+		
+		//English characters with lowercases and uppercases and numbers
+		size = 10;
+		password = PasswordUtils.getRandomPassword(size, true, true, false);
+		
+		assertNotNull(password);
+		assertTrue(password.length() == size);
+		
+		System.out.println("lower and upper English characters and numbers: " + password);
+		
+		//English characters with lowercases and uppercases, numbers and special characters
+		size = 10;
+		password = PasswordUtils.getRandomPassword(size, true, true, true);
+		
+		assertNotNull(password);
+		assertTrue(password.length() == size);
+		
+		System.out.println("lower and upper English characters, numbers and special characters: " + password);
 	}
 
 	@Test
