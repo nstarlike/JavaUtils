f3b2fba3039d3ff5e53840e903f50fb975c7868b
nstarlike <nstar0512@gmail.com>
2023-02-16 16:54:00 +0900

    make the skeleton of PasswordUtils

---
+++ src/main/java/nstarlike/javautils/credential/PasswordUtils.java
@@ -0,0 +1,23 @@
+package nstarlike.javautils.credential;
+
+public class PasswordUtils {
+	public static String getRandomPassword(int size, boolean hasNum, boolean hasCapital, boolean hasSpecial) {
+		String password = "";
+		return password;
+	}
+	
+	public static String encryptPassword(String password, String algorithm) {
+		String encrypted = "";
+		return encrypted;
+	}
+	
+	public static String decryptPassword(String password, String algorithm) {
+		String decrypted = "";
+		return decrypted;
+	}
+	
+	public static boolean isSafePassword(String password) {
+		boolean isSafe = false;
+		return isSafe;
+	}
+}
