31a68c9cc5eba724bc39fe673cbc8a7c60570c27
nstarlike <nstar0512@gmail.com>
2024-12-17 23:00:42 +0900

    Add an method to get recommended IDs.

--- src/main/java/nstarlike/utils/credential/IDUtils.java
+++ src/main/java/nstarlike/utils/credential/IDUtils.java
@@ -3,6 +3,7 @@ package nstarlike.utils.credential;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.List;
 
 public class IDUtils {
 	public static String getRandomID(String baseID) {
@@ -13,6 +14,11 @@ public class IDUtils {
 		}
 		
 		return baseID + sb.toString();
+	}
+	
+	public static List<String> getRecommendedIDs(IDRecommender iDRecommender, IDRefiner iDRefiner) {
+		List<String> ids = iDRecommender.getIds();
+		return iDRefiner.refine(ids);
 	}
 	
 	public static boolean checkID(String id, String regex) {
