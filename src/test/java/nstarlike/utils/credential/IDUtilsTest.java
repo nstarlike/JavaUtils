6a7f61c187185a326f4097a5f74a95ad4412a573
nstarlike <nstar0512@gmail.com>
2024-12-24 00:00:31 +0900

    Test the IDUtils.getRecommendedIDs method.

--- src/test/java/nstarlike/utils/credential/IDUtilsTest.java
+++ src/test/java/nstarlike/utils/credential/IDUtilsTest.java
@@ -2,6 +2,9 @@ package nstarlike.utils.credential;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +26,16 @@ public class IDUtilsTest {
 		boolean ret = IDUtils.checkID(id, regex);
 		
 		assertTrue(ret);
+	}
+	
+	@Test
+	public void testGetRecommendedIDs() {
+		IDRecommender recommender = new FixIDRecommender("testid");
+		IDChecker checker = new DBIDChecker();
+		IDRefiner refiner = new DBIDRefiner(checker);
+		List<String> recommended = IDUtils.getRecommendedIDs(recommender, refiner);
+		
+		assertNotNull(recommended);
+		System.out.println("recommended=" + recommended.toString());
 	}
 }
