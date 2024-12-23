5c511c387d14597ccef5d61cc2042362214cc33c
nstarlike <nstar0512@gmail.com>
2024-12-23 23:54:27 +0900

    Fix the error while copying.

--- src/test/java/nstarlike/utils/credential/DBIDChecker.java
+++ src/test/java/nstarlike/utils/credential/DBIDChecker.java
@@ -1,14 +1,19 @@
 package nstarlike.utils.credential;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBIDChecker implements IDChecker {
 
 	@Override
 	public List<String> getCheckedIds(List<String> ids) {
-		List<String> exists = List.copyOf(ids);
-		exists.remove(0);
-		return exists;
+		List<String> checked = new ArrayList<String>();
+		for(int i=0; i<ids.size(); i++) {
+			if(i > 0) {
+				checked.add(ids.get(i));
+			}
+		}
+		return checked;
 	}
 
 }
