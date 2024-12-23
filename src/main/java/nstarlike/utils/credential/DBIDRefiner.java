c455c161e41fa6fd6efed2987d769ce72cb7ff07
nstarlike <nstar0512@gmail.com>
2024-12-23 23:55:26 +0900

    Fix the error while copying

--- src/main/java/nstarlike/utils/credential/DBIDRefiner.java
+++ src/main/java/nstarlike/utils/credential/DBIDRefiner.java
@@ -1,5 +1,6 @@
 package nstarlike.utils.credential;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBIDRefiner implements IDRefiner {
@@ -11,7 +12,11 @@ public class DBIDRefiner implements IDRefiner {
 	
 	@Override
 	public List<String> refine(List<String> ids) {
-		List<String> candidates = List.copyOf(ids);
+		List<String> candidates = new ArrayList<String>();
+		for(String id : ids) {
+			candidates.add(id);
+		}
+		
 		List<String> checkedIds = this.iDChecker.getCheckedIds(candidates);
 		candidates.removeAll(checkedIds);
 		return candidates;
